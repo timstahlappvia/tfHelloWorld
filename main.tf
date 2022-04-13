768d7a69e2ba338a636f0ef20bef1bbf436edb84
Tim Stahl <tim.stahl@appvia.io>
2022-04-13 16:24:59 -0400

    Added k8s and networking

--- main.tf
+++ main.tf
@@ -13,6 +13,47 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "tstahlResourceGroup"
+  name     = "tstahl-k8s-resources"
   location = "eastus"
+
+  tags = {
+    Environment = "Tim Stahl - K8s Testing"
+    Team        = "Sales"
+  }
+}
+
+resource "azurerm_virtual_network" "k8s-vnet" {
+    name                = "tstahl-network"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    address_space       = ["10.1.0.0/16"]
+    
+    tags = {
+        Environment = "Tim Stahl - K8s Testing"
+        Team        = "Sales"
+  }
+}
+
+resource "azurerm_subnet" "internal" {
+    name                    = "internal"
+    virtual_network_name    = azurerm_virtual_network.k8s-vnet.name
+    resource_group_name     = azurerm_resource_group.rg.name
+    address_prefix          = ["10.1.0.0/24"]
+
+    tags = {
+        Environment = "Tim Stahl - K8s Testing"
+        Team        = "Sales"
+  }
+}
+
+resource "azure_kubernetes_cluster" "cluster1" {
+    name                    = "tstahl-k8s-cluster1"
+    location                = azurerm_resource_group.rg.location
+    resource_group_name     = azurerm_resource_group.rg.name
+    dns_prefix              = "tstahl-k8s-cluster1"
+
+    tags = {
+        Environment = "Tim Stahl - K8s Testing"
+        Team        = "Sales"
+  }
 }
