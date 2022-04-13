ffc83042ce993de529f9399696ffb6c9e56f3e80
Tim Stahl <tim.stahl@appvia.io>
2022-04-13 15:42:22 -0400

    First TF Item

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~> 3.0.2"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
-    required_version = ">= 1.1.0"
+  }
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-    name        = "tstahlResourceGroup"
-    location    = "eastus"
+  name     = "tstahlResourceGroup"
+  location = "eastus"
 }
