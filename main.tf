3982c2d9b259af5aff27ca0eacc8caad54c382f5
Tim Stahl <tim.stahl@appvia.io>
2022-04-14 08:49:24 -0400

    Moved outputs where it was supposed to be

--- main.tf
+++ main.tf
@@ -38,15 +38,11 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   virtual_network_name = azurerm_virtual_network.k8s-vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefix       = ["10.1.0.0/24"]
+  address_prefixes     = ["10.1.0.0/24"]
 
-  tags = {
-    Environment = "Tim Stahl - K8s Testing"
-    Team        = "Sales"
-  }
 }
 
-resource "azure_kubernetes_cluster" "cluster1" {
+resource "azurerm_kubernetes_cluster" "cluster1" {
   name                = "tstahl-k8s-cluster1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -61,10 +57,5 @@ resource "azure_kubernetes_cluster" "cluster1" {
 
   identity {
     type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Tim Stahl - K8s Testing"
-    Team        = "Sales"
   }
 }
